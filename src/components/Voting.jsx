6ea814cb28a7a1ffa97244778184c122a119206c
Dustin Specker <dustinspecker@dustinspecker.com>
2015-11-19 19:42:40 -0600

    wip - add voting winner/votedFor

--- src/components/Voting.jsx
+++ src/components/Voting.jsx
@@ -4,13 +4,27 @@ export default React.createClass({
   getPair() {
     return this.props.pair || [];
   },
+  isDisabled() {
+    return !!this.props.hasVoted;
+  },
+  hasVotedFor(entry) {
+    return this.props.hasVoted === entry;
+  },
   render() {
-    return <div classname="voting">
-      {this.getPair().map(entry =>
-        <button key={entry} onClick={() => this.props.vote(entry)}>
-          <h1>{entry}</h1>
-        </button>
-      )}
+    return <div classname='voting'>
+      {this.props.winner ?
+        <div ref='winner'>Winner is {this.props.winner}!</div> :
+        this.getPair().map(entry =>
+          <button
+            key={entry}
+            disabled={this.isDisabled()}
+            onClick={() => this.props.vote(entry)}>
+            <h1>{entry}</h1>
+            {this.hasVotedFor(entry) ?
+              <div className='label'>Voted</div> :
+              null}
+          </button>
+        )}
     </div>;
   }
 });
