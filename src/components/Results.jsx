d68ac110e752c2e445a2005f259977beeec8be18
Dustin Specker <dustinspecker@dustinspecker.com>
2015-11-22 15:07:05 -0600

    feat(results): display winner when there is one

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,5 +1,6 @@
 import PureRenderMixin from 'react-addons-pure-render-mixin';
 import React from 'react';
+import Winner from './Winner';
 
 export default React.createClass({
   mixins: [PureRenderMixin],
@@ -14,25 +15,27 @@ export default React.createClass({
     return 0;
   },
   render() {
-    return <div className='results'>
-      <div className='tally'>
-        {this.getPair().map(entry =>
-          <div key={entry} className='entry'>
-            <h1>{entry}</h1>
-            <div className='voteCount'>
-              {this.getVotes(entry)}
+    return this.props.winner ?
+      <Winner ref='winner' winner={this.props.winner} /> :
+      <div className='results'>
+        <div className='tally'>
+          {this.getPair().map(entry =>
+            <div key={entry} className='entry'>
+              <h1>{entry}</h1>
+              <div className='voteCount'>
+                {this.getVotes(entry)}
+              </div>
             </div>
-          </div>
-        )}
-      </div>
-      <div className='management'>
-        <button
-          ref='next'
-          className='next'
-          onClick={this.props.next}>
-          Next
-        </button>
-      </div>
-    </div>;
+          )}
+        </div>
+        <div className='management'>
+          <button
+            ref='next'
+            className='next'
+            onClick={this.props.next}>
+            Next
+          </button>
+        </div>
+      </div>;
   }
 });
