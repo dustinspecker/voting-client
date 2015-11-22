318de13a7c0fd25e2fe439e29d8124e58dff7cc5
Dustin Specker <dustinspecker@dustinspecker.com>
2015-11-22 14:51:43 -0600

    feat(results): display vote counts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,11 @@
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 import React from 'react';
 
 const pair = List.of('Trainspotting', '28 Days Later');
+const tally = Map({Trainspotting: 5, '28 Days Later': 4});
 
 export default React.createClass({
   render() {
-    return React.cloneElement(this.props.children, {pair});
+    return React.cloneElement(this.props.children, {pair, tally});
   }
 });
